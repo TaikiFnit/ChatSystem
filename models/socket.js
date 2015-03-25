b8131629f48d37b97bbc08f5762471793231025a
TaikiFnit <taikifnitspg@gmail.com>
2015-03-25 19:32:18 +0900

    source code format

--- models/socket.js
+++ models/socket.js
@@ -1,70 +1,51 @@
-
-function socketio(server){
-
+function socketio(server) {
 	//--- MySQL ---//
 	connection = require('./mysql.js')();
-    
-    // Catch data from Database and output console
-	connection.query('select * from messages', function(err, results, fields){
+	// Catch data from Database and output console
+	connection.query('select * from messages', function(err, results, fields) {
 		// output results		
 		console.log('Connection Test to MySQL in socket.js');
 		console.log('---results---');
 		console.log(results);
 		console.log('---result end---');
-		//console.log('---fields---');
-		//console.log(fields);
-		//console.log('---fields end---');
 	});
-    
 	//--- Socket.IO ---//
-
 	// wakeup socket.io server
 	io = require('socket.io')(server);
-
 	var connect_message = {
-		"name" : "NodejsServer",
-		"message" : "Success Connection"
+		"name": "NodejsServer",
+		"message": "Success Connection"
 	};
-
 	// クライアントとの接続が確立
 	io.sockets.on('connection', function(socket) {
-
 		// クライアントに接続完了のメッセージを送る
 		console.log('Emit: connect_message');
 		socket.emit('connect_message', connect_message);
-
 		// DBに格納されているすべてのデータを抽出し、それをクライアントに送る
-		connection.query('select * from messages', function(err, results){
-			// DBから取ってきたデータをコンソールに出力
-			console.log('Emit: bellow data for init_receive');
-			console.log(results);
-			console.log('--- data end ---');
-
+		connection.query('select * from messages', function(err, results) {
 			// クライアントにデータを送信
 			console.log('Emit: init_receive');
-			socket.emit('init_receive', results);	
+			socket.emit('init_receive', results);
 		});
-
 		// クライアントからメッセージが送られてきたときのイベント
 		socket.on('send', function(data) {
 			//　送られてきたデータをコンソールに出力
 			console.log('Event: send');
 			console.log(data);
 			console.log('---sended data end---')
-			
-			// 送られてきたデータをDBに挿入
-			connection.query('insert into messages(name, message) values(?, ?)', [data.name, data.message], function(err, results){
-				// SQL文を送った結果をコンソールに出力
-				console.log('--- insert result ---');
-				console.log(results);
-				console.log('--- insert result end ---');
-		
+				// 送られてきたデータをDBに挿入
+			connection.query('insert into messages(name, message) values(?, ?)', [
+				data.name, data.message
+			], function(err, results) {
 				// 送られてきたデータ(メッセージ)をすべてのクライアントに送る
-				console.log('Emit: receive');	
-				io.sockets.emit('receive', { id: results.insertId, name: data.name, message: data.message });
+				console.log('Emit: receive');
+				io.sockets.emit('receive', {
+					id: results.insertId,
+					name: data.name,
+					message: data.message
+				});
 			});
 		});
-
 		// 送られてきたidのメッセージをDBから削除するイベント
 		socket.on('SeDelete', function(data) {
 			// 送られてきたデータをコンソールに出力
@@ -72,37 +53,26 @@ function socketio(server){
 			console.log('output data from cliant');
 			console.log(data);
 			console.log('---data end---')
-
-			// 送られてきたidのコメントをDBから削除する
-			connection.query('delete from messages where id = ?', [data.id], function(err, results){
-				// DBからdeleteした結果を出力
-				console.log('--- delete results ---');
-				console.log(results);
-				console.log('--- delete results end ---');
-			
-				// クライアントにdeleteを要求
-				io.sockets.emit('ClDelete', {id: parseInt(data.id)});
-			});
+				// 送られてきたidのコメントをDBから削除する
+			connection.query('delete from messages where id = ?', [data.id],
+				function(err, results) {
+					// クライアントにdeleteを要求
+					io.sockets.emit('ClDelete', {
+						id: parseInt(data.id)
+					});
+				});
 		});
-
 		// DBに格納されているすべてのデータを削除する	
 		socket.on('truncate', function(data) {
 			// truncateイベント発生
 			console.log('Event: truncate');
-
 			// DBのmessagesテーブルに格納されているすべてのデータを削除するSQL文
-			connection.query('truncate table messages', function(err, results){
-				// DBからturncateした結果を出力
-				console.log('--- truncate results ---');
-				console.log(results);
-				console.log('--- truncate results end ---');
-
+			connection.query('truncate table messages', function(err, results) {
 				// クライアントにdelete要求
 				io.sockets.emit('truncate');
 			});
-		});	
+		});
 	});
 	//--- End Socket.IO ---//
 };
-
 module.exports = socketio;
--- routes/index.js
+++ routes/index.js
@@ -1,145 +1,124 @@
 var express = require('express');
 var router = express.Router();
-
 // Create Connection to MySQL
-connection = require('../models/mysql.js')();
-
+var connection = require('../models/mysql.js')();
 // Catch data from Database and output console
-connection.query('select * from messages', function(err, results, fields){
-		// output results		
-		console.log('Connection Test to MySQL from messages in index.js');
-		console.log('---results---');
-		console.log(results);
-		console.log('---result end---');
-		//console.log('---fields---');
-		//console.log(fields);
-		//console.log('---fields end---');
+connection.query('select * from messages', function(err, results, fields) {
+	// output results		
+	console.log('Connection Test to MySQL from messages in index.js');
+	console.log('---results---');
+	console.log(results);
+	console.log('---result end---');
 });
-
 // Catch data from Database and output console
-connection.query('select * from users', function(err, results, fields){
+connection.query('select * from users', function(err, results, fields) {
 	// output results		
 	console.log('Connection Test to MySQL from users in index.js');
 	console.log('---results---');
 	console.log(results);
 	console.log('---result end---');
-	//console.log('---fields---');
-	//console.log(fields);
-	//console.log('---fields end---');
 });
-
 //--- Routing of browser ---//
-
-var loginCheck = function(req, res, next){
-    
-  if(req.session.user){
-      console.log('red if');
-    next();
-  } else {
-      console.log('rredirect login');
-    res.redirect('/login');
-  }
+var loginCheck = function(req, res, next) {
+	if (req.session.user) {
+		console.log('red if');
+		next();
+	} else {
+		console.log('rredirect login');
+		res.redirect('/login');
+	}
 };
-
 // output console request
-var output = function(req, res, next){
+var output = function(req, res, next) {
 	/*console.log('--- variable req ---');
 	console.log(req);
 	console.log('--- end req ---');*/
-
 	console.log('--- variable req.body ---');
 	console.log(req.body);
 	console.log('--- end req.body ---');
-
 	next();
 };
-
 // チャット画面をrend
 router.get('/', output, loginCheck, function(req, res) {
-    console.log("debug on /");
-    console.log(req.session.user);
-    res.render('index', { user: req.session.user });
+	console.log("debug on /");
+	console.log(req.session.user);
+	res.render('index', {
+		user: req.session.user
+	});
 });
-
 // ログイン処理を行うlogin.html
-router.get('/login', output, function(req, res){
-    
-    console.log("debug on /login");
-    console.log(req.session.user);
-    
-    
-    if(req.session.user){
-        console.log('session redirect!');
-        res.redirect('/');
-    }
-    else{
-        res.render('login', {err: ''});
-    }
+router.get('/login', output, function(req, res) {
+	console.log("debug on /login");
+	console.log(req.session.user);
+	if (req.session.user) {
+		console.log('session redirect!');
+		res.redirect('/');
+	} else {
+		res.render('login', {
+			err: ''
+		});
+	}
 });
-
 // browser用のログイン処理
-router.post('/login', output, function(req, res){
-	
-	connection.query('select * from users where (name = ?) and (password = ?)', [req.body.name, req.body.password], function(err, results){
-
-        if(err){
-           // エラー時の処理
-            console.log(err);
-        }
-	// ユーザー名とパスワードが一致(resultsに値が格納されている)
-         if(results.toString() !== ''){
-             req.session.user = req.body.name;
-             res.redirect('/');
-         } else {
-             res.render('login', {err: ' has-error'});
-         }
-         
-        console.log("--- results of login ---");
-        console.log(results);
-        console.log("--- end results ---");
-    });
+router.post('/login', output, function(req, res) {
+	connection.query('select * from users where (name = ?) and (password = ?)', [
+		req.body.name, req.body.password
+	], function(err, results) {
+		if (err) {
+			// エラー時の処理
+			console.log(err);
+		}
+		// ユーザー名とパスワードが一致(resultsに値が格納されている)
+		if (results.toString() !== '') {
+			req.session.user = req.body.name;
+			res.redirect('/');
+		} else {
+			res.render('login', {
+				err: ' has-error'
+			});
+		}
+		console.log("--- results of login ---");
+		console.log(results);
+		console.log("--- end results ---");
+	});
 });
-
 // ユーザーの登録を行うPOSTの処理
-router.post('/add', output, function(req, res){
-
-  /* mysql */
-  connection.query('insert into users(name, password) values(?, ?)', [req.body.name, req.body.password], function(err, results){
-      if(err){
-        // エラー時の処理
-        console.log(err);
-        res.redirect('back');
-      } else {
-          // セッションも一緒に送る
-          req.session.user = req.body.name;
-          res.redirect('/');
-      }
-  });
+router.post('/add', output, function(req, res) {
+	/* mysql */
+	connection.query('insert into users(name, password) values(?, ?)', [req.body
+		.name, req.body.password
+	], function(err, results) {
+		if (err) {
+			// エラー時の処理
+			console.log(err);
+			res.redirect('back');
+		} else {
+			// セッションも一緒に送る
+			req.session.user = req.body.name;
+			res.redirect('/');
+		}
+	});
 });
-
-router.get('/logout', output, function(req, res){
-  req.session.destroy();
-  console.log('deleted session');
-  res.redirect('/');
+router.get('/logout', output, function(req, res) {
+	req.session.destroy();
+	console.log('deleted session');
+	res.redirect('/');
 });
-
-
 // Routing of Application
-
 // アカウント登録
 router.post('/appCreate', output, function(req, res) {
 	// output request data
 	console.log(req.body);
-    var name = req.body.name;
-    var password = req.body.password;
-   
-    connection.query('insert into users(name, password) values(?, ?)', [name, password], function(err, results){
-		var checkResult = function(re, er){
-            if(er){
-                return false;   
-            }
-            
-			if(re.toString() !== ''){
+	var name = req.body.name;
+	var password = req.body.password;
+	connection.query('insert into users(name, password) values(?, ?)', [name,
+		password
+	], function(err, results) {
+		var checkResult = function(re, er) {
+			if (er) {
+				return false;
+			}
+			if (re.toString() !== '') {
 				console.log('in if');
 				return true;
 			} else {
@@ -147,36 +126,32 @@ router.post('/appCreate', output, function(req, res) {
 				return false;
 			}
 		};
-
 		var response = {
 			"result": r = checkResult(results, err),
 			"err": err
 		};
-        
-        // output variable err
+		// output variable err
 		console.log('--- err ---');
 		console.log(err);
 		console.log('--- err end ---');
-        
 		req.session.user = req.body.name;
 		res.send(response);
-    });
+	});
 });
-
 // Login for application
-router.post('/appLogin', output, function(req, res){
+router.post('/appLogin', output, function(req, res) {
 	var name = req.body.name;
 	var password = req.body.password;
 	console.log('name : ' + name);
 	console.log('password : ' + password);
-
-	connection.query('select * from users where (name = ?) and (password = ?)', [name, password], function(err, results){      
-        
-		var checkResult = function(re, er){
-            if(er){
-                return false;
-            }
-			if(re.toString() !== ''){
+	connection.query('select * from users where (name = ?) and (password = ?)', [
+		name, password
+	], function(err, results) {
+		var checkResult = function(re, er) {
+			if (er) {
+				return false;
+			}
+			if (re.toString() !== '') {
 				console.log('in if');
 				return true;
 			} else {
@@ -184,64 +159,55 @@ router.post('/appLogin', output, function(req, res){
 				return false;
 			}
 		};
-
 		var response = {
 			"result": r = checkResult(results, err),
 			"err": err
 		};
-        
-        // output variable err
+		// output variable err
 		console.log('--- err ---');
 		console.log(err);
-		console.log('--- err end ---');  
-        
+		console.log('--- err end ---');
 		req.session.user = req.body.name;
 		res.send(response);
 	});
-    
 });
-
 // セッションのチェック
-router.get('/checkSession', output, function(req, res){
+router.get('/checkSession', output, function(req, res) {
 	console.log("checkSessionにGETされました。");
-
 	console.log("--- req ---");
 	//console.log(req);
 	console.log("--- end req ---");
-	
 	console.log("--- res ---");
 	//console.log(res);
 	console.log("--- end res ---");
-    
-    console.log("--- req.headers ---");
+	console.log("--- req.headers ---");
 	console.log(req.headers);
 	console.log("--- end req.headers ---");
-
-    console.log("--- req.headers.sessionid ---");
-    console.log(req.headers.sessionid);
-    console.log("--- end req.headers.sessionid ---");
-
-   	if(req.headers.sessionid){
+	console.log("--- req.headers.sessionid ---");
+	console.log(req.headers.sessionid);
+	console.log("--- end req.headers.sessionid ---");
+	if (req.headers.sessionid) {
 		console.log("trueを送信します.");
 		// セッションが有効
-		res.send({"result": true});
-	} 
-	else {
+		res.send({
+			"result": true
+		});
+	} else {
 		console.log("falseを送信します.");
 		// セッションが無効
-		res.send({"result": false});
-	}	
+		res.send({
+			"result": false
+		});
+	}
 });
-
 // ログアウト
-router.get('/appLogout', output, function(req, res){
-  req.session.destroy();
-  console.log('deleted session');
-  // セッションを無効にしたことを通知
-  var response = {
-    "result": true
-  };
-  res.send(response);
+router.get('/appLogout', output, function(req, res) {
+	req.session.destroy();
+	console.log('deleted session');
+	// セッションを無効にしたことを通知
+	var response = {
+		"result": true
+	};
+	res.send(response);
 });
-
 module.exports = router;
